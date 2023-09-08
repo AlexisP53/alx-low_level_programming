39e1bd24b66854563a36ef03d3410cb09ae909e3
alexis <alexisalezandre53@gmail.com>
2023-09-08 11:49:48 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is c programming language
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
