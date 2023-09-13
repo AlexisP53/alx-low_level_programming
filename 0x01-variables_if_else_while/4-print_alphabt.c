901f35678eef2d5fc790893f7f661af2011d2ff3
alexis <alexisalezandre53@gmail.com>
2023-09-13 02:08:28 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,17 +2,24 @@
 
 
 /**
- * main - program that prints the alphabet in lowercase
+ * main - program that prints the alphabet in lowercase and upper case
  *
- *  Return: Always 0.
+ * Return: Always 0.
  */
+
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
 	putchar(letter);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
 
