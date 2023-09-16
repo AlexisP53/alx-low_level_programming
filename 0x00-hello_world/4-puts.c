ebd0240b26baded2fa0a3570e58dc9cef44d0e02
alexis <alexisalezandre53@gmail.com>
2023-09-16 00:23:23 -0700

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
-*/
+ * main - this is the main put function
+ * Return: 0
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
+
